7ac4da7dc3a9a0ed721ff89acfb016d9be5c9be3
Salasian <salasiandev@gmail.com>
2022-08-23 19:24:37 -0700

    Función de strArea y strTags añadida

--- src/displayDishes.js
+++ src/displayDishes.js
@@ -4,13 +4,54 @@ import getElement from './getElement.js';
 //Obtiene el contenedor de los platillos
 const dishesContainer = getElement('row');
 
+//Asignamos la dirección url de la API a una variable constante
+const SINGLE_DISH_API = 'https://www.themealdb.com/api/json/v1/1/lookup.php';
+
+//Función que solicita el elemento a la API para obtener sus atributos mediante fetch usando el id como endpoint
+const fetchDish = async (id) => {
+  //Hacemos fetch a la API con la id puesta en parámetros
+  const response = await fetch(`${SINGLE_DISH_API}?i=${id}`);
+  //Transformamos los datos a el formato json
+  const { meals } = await response.json();
+  //Regresamos el arreglo de meals que contiene todos los atributos del platiollo
+  return meals[0];
+};
+
+//Función que llama a cada platillo de la categoría y toma los atributos del mismo para obtener los atributos individuales y almacenarlos en un arreglo en forma de objeto
+const constructedDishesObj = async (dishes) => {
+  //Inicializamos la variable donde guardaremos los objetos
+  const constructedDishes = [];
+  //Recorreremos el arreglo otorgado en parámetros
+  for (const dish of dishes) {
+    //Obtendremos el id de el platillo
+    const { idMeal: id } = dish;
+    //Obtendremos los la response con los atributos necesarios de un solo platillo
+    const singleDish = await fetchDish(id);
+    //Deconstruye el arreglo y asigna las nuevas variables
+    const { strArea, strTags } = singleDish;
+    //Añade un nuevo objeto al arreglo con todos los atributos dados en categoria y los atributos de strArea y strTags y formar un nuevo objeto
+    constructedDishes.push({ ...dish, strArea, strTags });
+  }
+  //Regresamos el arreglo con cada uno de los elementos nuevos
+  return constructedDishes;
+};
+
 //Función que despliega los platillos mediante el uso del arreglo dado en parámetros y los asigna a el contenedor para desplegarlos en pantalla
-export function displayDishes(dishes) {
+export async function displayDishes(dishes) {
+  //Usa la función para obtener el arreglo de los platillos con sus correspondientes atributos
+  const constructedDishes = await constructedDishesObj(dishes);
+
   //Guarda la lista de elementos en formato de un solo string y en formato html para desplegarlos en pantalla
-  const dishesHTML = dishes
+  const dishesHTML = constructedDishes
     .map((dish) => {
+      const {
+        idMeal: id,
+        strMealThumb: img,
+        strMeal: name,
+        strArea,
+        strTags,
+      } = dish;
       //Desconstrucción de valores de la variable dish
-      const { idMeal: id, strMealThumb: img, strMeal: name } = dish;
       const formattedPrice = new Intl.NumberFormat('ja-JP', {
         style: 'currency',
         currency: 'MEX',
@@ -22,7 +63,7 @@ export function displayDishes(dishes) {
                 <img src="${img}" class="dish-img" alt="dish" />
                 <span class="dish-left">Quedan ${id % 10}</span>
                 <div class="dish-info">
-                  Banquete Providencia <br /><span class="yellow-text"
+                  ${strArea} <br /><span class="yellow-text"
                     >${name}</span
                   >
                 </div>
@@ -42,6 +83,7 @@ export function displayDishes(dishes) {
     })
     .join('');
   //Se asignará la estructura al contenedor principal y representará los platillos correspondientes
+
   dishesContainer.innerHTML = dishesHTML;
 }
 
